597546d6020bae19df3d020501038f23cf41d852
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-22 09:56:18 +0100

    Add server setup with environment configuration and database connection

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////
+// DO NOT CHANGE THE ORDER OF THE IMPORTS;
+// DOT ENV AND MODULE ALIAS WILL NOT WORK PROPERLY UNLESS THEY ARE IMPORTED FIRST
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+if (process.env.NODE_ENV === 'production') {
+    require('module-alias/register');
+}
+
+///////////////////////////////////////////////////////////////////////
+
+import http from "http";
+
+import {db} from "@/db";
+import {ENVIRONMENT} from "@/common";
+
+
+
+dotenv.config();
+
+
+const appName= ENVIRONMENT.APP.NAME
+const port = ENVIRONMENT.APP.PORT;
+
+const server = http.createServer((req, res) => {})
+
+const appServer=server.listen(port, async ()=>{
+    await db()
+
+    console.log(`=> ${appName} app listening on port ${port}!`)
+});
