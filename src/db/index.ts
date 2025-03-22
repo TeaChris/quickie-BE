2bb8353c8c78d59bb552ba51fe1cd1f11e111917
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-03-22 09:56:50 +0100

    Add database connection module with custom options

---
+++ src/db/index.ts
@@ -0,0 +1,23 @@
+import * as mongoose from "mongoose";
+import {ConnectOptions} from "mongoose";
+
+import {ENVIRONMENT} from "@/common";
+
+interface CustomConnectOptions extends ConnectOptions {
+    maxPoolSize?: number;
+    minPoolSize?: number;
+}
+
+export const db = async (): Promise<void> => {
+    try {
+        const connect = await mongoose.connect(ENVIRONMENT.DB.URL, {
+            minPoolSize: 100,
+            maxPoolSize: 100,
+        } as CustomConnectOptions);
+
+        console.log("MongoDB Connected to " + connect.connection.name);
+    } catch (error) {
+        console.error('Error:' + (error as Error).message);
+        process.exit(1);
+    }
+}
